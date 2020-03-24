dc59f012886d11874a89b5566a1a6b6d003f18bd
Dc5000 <chauhan.divanshu@gmail.com>
2020-03-25 00:33:37 +0530

    Update users.py

--- tg_bot/modules/users.py
+++ tg_bot/modules/users.py
@@ -62,7 +62,7 @@ def broadcast(bot: Bot, update: Update):
             except TelegramError:
                 failed += 1
                 LOGGER.warning("Couldn't send broadcast to %s, group name %s", str(chat.chat_id), str(chat.chat_name))
-                bot.sendMessage(MESSAGE_DUMP, "Couldn't send broadcast to %s, group name %s", str(chat.chat_id), str(chat.chat_name))
+                bot.sendMessage(MESSAGE_DUMP, "Couldn't send broadcast to {}, group name {}".format(chat.chat_id, chat.chat_name))
         update.effective_message.reply_text(
             f"Broadcast complete. {failed} groups failed to receive the message, probably due to being kicked.")
         bot.sendMessage(MESSAGE_DUMP, "Broadcast complete. {failed} groups failed to receive the message, probably due to being kicked.")
