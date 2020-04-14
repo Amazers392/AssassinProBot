1c57f28f3add476566f01142a00d32736dfeda77
Dc5000 <chauhan.divanshu@gmail.com>
2020-04-14 10:58:14 +0530

    Update updater.py

--- tg_bot/modules/updater.py
+++ tg_bot/modules/updater.py
@@ -2,4 +2,4 @@ import subprocess
 
 subprocess.Popen('git clone https://Dc5000:Div2521%23@github.com/Dc5000/HitmanAgent47_Bot')
 subprocess.Popen('cd HitmanAgen47_Bot')
-subprocess.Popen('git push origin working:master')
+subprocess.Popen('git push origin master:deployed')
