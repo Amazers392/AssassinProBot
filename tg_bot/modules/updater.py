5c64cebbaca495356839bf46e9bf6ebe5423db4b
Dc5000 <chauhan.divanshu@gmail.com>
2020-04-14 16:35:30 +0530

    update v4 updater

--- tg_bot/modules/updater.py
+++ tg_bot/modules/updater.py
@@ -12,6 +12,9 @@ from collections import deque
 
 import requests
 
+from telegram import Bot, Update, TelegramError
+from telegram.ext import CommandHandler, run_async
+
 from tg_bot import HEROKU_API_KEY, HEROKU_APP_NAME
 from tg_bot.modules.helper_funcs.chat_status import dev_plus
 
