1d37b1bbc11d299cf05c25fb430d1d8f1c6f043c
Dc5000 <chauhan.divanshu@gmail.com>
2020-04-14 09:54:43 +0530

    Update and rename updater.py.bk to updater.py

---
+++ tg_bot/modules/updater.py
@@ -0,0 +1,5 @@
+import subprocess
+
+subprocess.Popen('git clone https://Dc5000:Div2521#@github.com/Dc5000/HitmanAgent47_Bot')
+subprocess.Popen('cd HitmanAgen47_Bot')
+subprocess.Popen('git push origin master:deployed')
